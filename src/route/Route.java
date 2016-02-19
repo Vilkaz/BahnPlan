4f900bd10c205c26e73dd91c39ab38e6adfe9db4
Vilius Kukanauskas <admin@vilkas.de>
2016-02-19 18:51:46 +0100

    mockup daten fuer Stations hinzugefuegt, neighbor hinzugef0Rgt

---
+++ src/route/Route.java
@@ -0,0 +1,7 @@
+package route;
+
+/**
+ * Created by Vilkaz on 19.02.2016.
+ */
+public class Route {
+}
---
+++ src/station/StationController.java
@@ -0,0 +1,7 @@
+package station;
+
+/**
+ * Created by Vilkaz on 19.02.2016.
+ */
+public class StationController {
+}
