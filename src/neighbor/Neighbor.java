cf5c8b43690ff3549050f8019d52fdcc39acab6e
Vilius Kukanauskas <admin@vilkas.de>
2016-02-19 19:09:25 +0100

    Neighbour variable umgenannt, getter gesetzt

--- src/neighbor/Neighbor.java
+++ src/neighbor/Neighbor.java
@@ -4,11 +4,24 @@ package neighbor;
  * Created by Vilkaz on 19.02.2016.
  */
 public class Neighbor {
-    int range;
+    int distance;
     int id;
 
     public Neighbor(int range, int id) {
-        this.range = range;
+        this.distance = range;
         this.id = id;
     }
+
+    //region getter and setter
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    //region getter
 }
